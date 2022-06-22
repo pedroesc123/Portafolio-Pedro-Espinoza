743487bd7caccd5c23908c88d089cfb1572314c2
Pedro Espinoza <pedroesc.9905@gmail.com>
2022-06-21 19:24:39 -0500

    Arreglado los estilos

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import twitter from '../img/logo-twitter.jpg'
+import paises from '../img/banderas-paises.jpg'
 import '../Styles/Portfolio.css'
 import { Link } from "react-router-dom";
 
@@ -15,8 +16,8 @@ const Portfolio = () => {
                             Info de Paises
                         </Link>
                     </h1>
-                    <img src={twitter} alt="Imagen del proyecto" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi nobis fuga beatae praesentium tenetur reiciendis.</p>
+                    <img src={paises} alt="Imagen del proyecto" />
+                    <p>En esta aplicación se extrajo la información de una API y se mostro a todos los países con algunas características de ellos, además de mostrarnos un ranking de los que presentan mayor población.</p>
                 </div>
                 <div className="card-container">
                     <h1>Project 1</h1>
--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -42,6 +42,7 @@ const Resume = () => {
                         <ul>
                             <li>HTML</li>
                             <li>CSS</li>
+                            <li>JavaScript</li>
                             <li>React</li>
                             <li>Python</li>
                             <li>C/C++</li>
