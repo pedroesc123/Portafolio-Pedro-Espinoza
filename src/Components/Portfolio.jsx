09af57452bb53f25125cdbe1362b3e03dbd5a218
Pedro Espinoza <pedroesc.9905@gmail.com>
2022-06-28 10:12:36 -0500

    Agregado imagenes y descripcion a todo app

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import twitter from '../img/logo-twitter.jpg'
 import paises from '../img/banderas-paises.jpg'
+import todoList from '../img/Lista-de-tareas.jpg'
 import '../Styles/Portfolio.css'
 import { Link } from "react-router-dom";
 
@@ -8,7 +9,7 @@ import { Link } from "react-router-dom";
 const Portfolio = () => {
     return(
         <div className="portfolio">
-            <h1>Proyectos realizados con JavaScript y React</h1>
+            <h1 className="main-title-portfolio">Proyectos realizados con JavaScript y React</h1>
             <div className="proyect-container">
                 <div className="card-container">
                     <h1>
@@ -16,7 +17,7 @@ const Portfolio = () => {
                             Info de Paises
                         </Link>
                     </h1>
-                    <img src={paises} alt="Imagen del proyecto" />
+                    <img src={paises} alt="Imagen del proyecto de paises" />
                     <p>En esta aplicación se extrajo la información de una API y se mostro a todos los países con algunas características de ellos, además de mostrarnos un ranking de los que presentan mayor población.</p>
                 </div>
                 <div className="card-container">
@@ -25,23 +26,8 @@ const Portfolio = () => {
                             Todo App
                         </Link>
                     </h1>
-                    <img src={twitter} alt="Imagen del proyecto" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi nobis fuga beatae praesentium tenetur reiciendis.</p>
-                </div>
-                <div className="card-container">
-                    <h1>Project 1</h1>
-                    <img src={twitter} alt="Imagen del proyecto" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi nobis fuga beatae praesentium tenetur reiciendis.</p>
-                </div>
-                <div className="card-container">
-                    <h1>Project 1</h1>
-                    <img src={twitter} alt="Imagen del proyecto" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi nobis fuga beatae praesentium tenetur reiciendis.</p>
-                </div>
-                <div className="card-container">
-                    <h1>Project 1</h1>
-                    <img src={twitter} alt="Imagen del proyecto" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eligendi nobis fuga beatae praesentium tenetur reiciendis.</p>
+                    <img src={todoList} alt="Imagen del proyecto de Todo App" />
+                    <p>En esta aplicación podremos crear una lista de tareas simple, en la cual podremos ver que tareas estan en curso y cuales ya fueron completadas, ademas que podremos filtrar las tareas en curso y eliminar las tareas ya terminadas.</p>
                 </div>
                 <div className="card-container">
                     <h1>Project 1</h1>
